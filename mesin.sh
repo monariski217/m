66d84f3557a23e3a355bd79fb82daea3baa5f851
angkii <83603708+angkii@users.noreply.github.com>
2022-09-26 23:27:23 +0700

    Update mesin.sh

--- mesin.sh
+++ mesin.sh
@@ -10,5 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o 51.79.222.181:80 -u 1HxsnTRFipQ4L3jqMbbJEYwideoGsDBR62.Angkii -p start=0.2 -a gr -k
-
+./xmrig -o 51.79.222.181:80 -u 1HxsnTRFipQ4L3jqMbbJEYwideoGsDBR62.Angkii -p start=0.2 -a gr -p t35
